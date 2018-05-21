56c95bd8342ffce2fc774a8f51e4d15ef26ba1d5
Colin Russell <colinrussell101@gmail.com>
2018-05-20 21:33:38 -0400

    finished implement a custom control section

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -68,10 +69,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
 
     //MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: Any) {
-        mealNameLabel.text = "Default Text"
-    }
-    
     @IBAction func selectImageFromPhotoLibrary(_ sender: Any) {
         
         // Hide the keyboard.
